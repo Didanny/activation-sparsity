eeaee318a3695bd1e02a55d37d9ec58faf96cb7f
Didanny <daa50@mail.aub.edu>
2025-03-02 22:44:15 -0800

    Fix slurm scripts

--- slurm/vgg_cifar100.sh
+++ slurm/vgg_cifar100.sh
@@ -3,8 +3,8 @@
 #SBATCH -N 1 
 #SBATCH -p opengpu.p 
 #SBATCH --gres=gpu:1 
-#SBATCH -o slurm_logs/log_cifar100_resnet.out 
-#SBATCH -e slurm_logs/err_cifar100_resnet.out
+#SBATCH -o slurm_logs/log_cifar100_vgg.out 
+#SBATCH -e slurm_logs/err_cifar100_vgg.out
 
 python train.py --model vgg11_bn --dataset cifar100
 python train.py --model vgg13_bn --dataset cifar100
